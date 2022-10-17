9680f771aaaca806daca70a3f1ed674d02225f9f
Wesley Kelly <66736080+wesleykel@users.noreply.github.com>
2022-10-17 15:45:12 +0100

    data removed

--- api/db/connection.js
+++ api/db/connection.js
@@ -19,20 +19,3 @@ port:process.env.PGPORT,
 
 export default pool
 
-/*USER="wesley"
-PGHOST="localhost"
-PGPASSWORD="root"
-PGDATABASE="postgres"
-PGPORT=5432*/
-//DATABASE_URL=postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}
-/*PGDATABASE=railway
-PGHOST=containers-us-west-52.railway.app
-PGPASSWORD=lAOtc5UyUs51GUCd8xE0
-PGPORT=7198
-PGUSER=postgres
-
-PGUSER="wesley"
-PGHOST="localhost"
-PGPASSWORD="root"
-PGDATABASE="postgres"
-PGPORT=5432*/
